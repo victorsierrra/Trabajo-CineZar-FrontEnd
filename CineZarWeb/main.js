e032f54edc4e22801f195567f17e3c646b7ee19a
Victor <a27300@svalero.com>
2024-11-07 13:43:57 +0100

    Creacion Carpeta Cliente - Prueba Fetch

---
+++ CineZarWeb/main.js
@@ -0,0 +1,16 @@
+window.onload = function (e) {
+    console.log("Documento Cargado")
+    let promise = fetch("https://localhost:7165/api/Pelicula")
+    promise.then(res => res.json())
+        .then(data => {
+            console.log(data)
+            data.forEach(p => {
+                let node = document.createElement('div')
+                node.innerHTML = `<h2>${p.titulo}</h1>
+                <p>${p.sinopsis}<p>
+                `
+                document.getElementById('resultado').appendChild(node)
+            });
+        })
+        .catch()
+}
