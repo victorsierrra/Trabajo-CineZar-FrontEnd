7bc8e0aebf8296a8d8bca5cc328adfa4b4df106c
Victor <a27300@svalero.com>
2024-11-14 13:09:15 +0100

    Enmaquetación del mapa interactivo de asientos

--- CineZarWeb/MapaAsientos/main.js
+++ CineZarWeb/MapaAsientos/main.js
@@ -9,14 +9,33 @@ window.onload = function (e) {
 }
 
 function imprimirAsientos() {
-    const filaA = document.getElementById('fila-A')
-    const filaB = document.getElementById('fila-B')
-    const filaC = document.getElementById('fila-C')
-    const filaD = document.getElementById('fila-D')
-    const filaE = document.getElementById('fila-E')
-    const filaF = document.getElementById('fila-F')
-    const filaG = document.getElementById('fila-G')
-    const filaH = document.getElementById('fila-H')
+    const filaA = document.getElementById('fila-A');
+    const filaB = document.getElementById('fila-B');
+    const filaC = document.getElementById('fila-C');
+    const filaD = document.getElementById('fila-D');
+    const filaE = document.getElementById('fila-E');
+    const filaF = document.getElementById('fila-F');
+    const filaG = document.getElementById('fila-G');
+    const filaH = document.getElementById('fila-H');
+    const filaI = document.getElementById('fila-I');
+    const filaJ = document.getElementById('fila-J');
+    const filaK = document.getElementById('fila-K');
+    const filaL = document.getElementById('fila-L');
+    const filaM = document.getElementById('fila-M');
+    const filaN = document.getElementById('fila-N');
+    const filaO = document.getElementById('fila-O');
+    const filaP = document.getElementById('fila-P');
+    const filaQ = document.getElementById('fila-Q');
+    const filaR = document.getElementById('fila-R');
+    const filaS = document.getElementById('fila-S');
+    const filaT = document.getElementById('fila-T');
+    const filaU = document.getElementById('fila-U');
+    const filaV = document.getElementById('fila-V');
+    const filaW = document.getElementById('fila-W');
+    const filaX = document.getElementById('fila-X');
+    const filaY = document.getElementById('fila-Y');
+    const filaZ = document.getElementById('fila-Z');
+    
     try {
         let promise = fetch("https://localhost:7165/api/Sesion")
         promise.then(response => response.json())
@@ -31,34 +50,88 @@ function imprimirAsientos() {
                     circulo.innerText = asiento.numero
                     switch (asiento.fila) {
                         case 'A':
-                            filaA.appendChild(circulo)
+                            filaA.appendChild(circulo);
                             break;
                         case 'B':
-                            filaB.appendChild(circulo)
+                            filaB.appendChild(circulo);
                             break;
                         case 'C':
-                            filaC.appendChild(circulo)
+                            filaC.appendChild(circulo);
                             break;
                         case 'D':
-                            filaD.appendChild(circulo)
+                            filaD.appendChild(circulo);
                             break;
                         case 'E':
-                            filaE.appendChild(circulo)
+                            filaE.appendChild(circulo);
                             break;
                         case 'F':
-                            filaF.appendChild(circulo)
+                            filaF.appendChild(circulo);
                             break;
                         case 'G':
-                            filaG.appendChild(circulo)
+                            filaG.appendChild(circulo);
                             break;
                         case 'H':
-                            filaH.appendChild(circulo)
+                            filaH.appendChild(circulo);
+                            break;
+                        case 'I':
+                            filaI.appendChild(circulo);
+                            break;
+                        case 'J':
+                            filaJ.appendChild(circulo);
+                            break;
+                        case 'K':
+                            filaK.appendChild(circulo);
+                            break;
+                        case 'L':
+                            filaL.appendChild(circulo);
+                            break;
+                        case 'M':
+                            filaM.appendChild(circulo);
+                            break;
+                        case 'N':
+                            filaN.appendChild(circulo);
+                            break;
+                        case 'O':
+                            filaO.appendChild(circulo);
+                            break;
+                        case 'P':
+                            filaP.appendChild(circulo);
+                            break;
+                        case 'Q':
+                            filaQ.appendChild(circulo);
+                            break;
+                        case 'R':
+                            filaR.appendChild(circulo);
+                            break;
+                        case 'S':
+                            filaS.appendChild(circulo);
+                            break;
+                        case 'T':
+                            filaT.appendChild(circulo);
+                            break;
+                        case 'U':
+                            filaU.appendChild(circulo);
+                            break;
+                        case 'V':
+                            filaV.appendChild(circulo);
+                            break;
+                        case 'W':
+                            filaW.appendChild(circulo);
+                            break;
+                        case 'X':
+                            filaX.appendChild(circulo);
+                            break;
+                        case 'Y':
+                            filaY.appendChild(circulo);
+                            break;
+                        case 'Z':
+                            filaZ.appendChild(circulo);
                             break;
                         default:
-                            console.log('No se encuentra la fila')
-                    }
+                            console.log('No se encuentra la fila');
+                    }                    
                     if (asiento.comprado === true) {
-                        circulo.style.backgroundColor = "gray"
+                        circulo.style.backgroundColor = "#1F293D"
                         circulo.style.cursor = "not-allowed"
                     }
                 });
@@ -68,8 +141,17 @@ function imprimirAsientos() {
     }
 }
 function seleccionarAsiento(id) {
+    let asientoSeleccionado = document.getElementById(id)
     let variable = id.substring(8)
+    if(asientosSeleccionados.includes(variable))
+    {
+        console.log('Dejar de seleccionar')
+        asientosSeleccionados = asientosSeleccionados.filter(asiento => asiento !=variable)
+    }
+    else{
     asientosSeleccionados.push(variable)
+    }
+    asientoSeleccionado.classList.toggle('selected')
 
     console.log(`Clog -------------    ${variable}`)
     console.log(asientosSeleccionados)
