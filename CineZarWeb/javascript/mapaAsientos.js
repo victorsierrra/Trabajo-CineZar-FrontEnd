1f29360ae1ee42df81faa2cc430d728e3ca17524
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-21 19:35:48 +0100

    Cambio de tipo de impresión de los asientos

--- CineZarWeb/javascript/mapaAsientos.js
+++ CineZarWeb/javascript/mapaAsientos.js
@@ -25,32 +25,7 @@ function verFecha(id)
     })
 }
 function imprimirAsientos() {
-    const filaA = document.getElementById('fila-A');
-    const filaB = document.getElementById('fila-B');
-    const filaC = document.getElementById('fila-C');
-    const filaD = document.getElementById('fila-D');
-    const filaE = document.getElementById('fila-E');
-    const filaF = document.getElementById('fila-F');
-    const filaG = document.getElementById('fila-G');
-    const filaH = document.getElementById('fila-H');
-    const filaI = document.getElementById('fila-I');
-    const filaJ = document.getElementById('fila-J');
-    const filaK = document.getElementById('fila-K');
-    const filaL = document.getElementById('fila-L');
-    const filaM = document.getElementById('fila-M');
-    const filaN = document.getElementById('fila-N');
-    const filaO = document.getElementById('fila-O');
-    const filaP = document.getElementById('fila-P');
-    const filaQ = document.getElementById('fila-Q');
-    const filaR = document.getElementById('fila-R');
-    const filaS = document.getElementById('fila-S');
-    const filaT = document.getElementById('fila-T');
-    const filaU = document.getElementById('fila-U');
-    const filaV = document.getElementById('fila-V');
-    const filaW = document.getElementById('fila-W');
-    const filaX = document.getElementById('fila-X');
-    const filaY = document.getElementById('fila-Y');
-    const filaZ = document.getElementById('fila-Z');
+    const mapa = document.querySelector('.mapa-asientos');
 
     console.log(idSesion)
     try {
@@ -66,88 +41,7 @@ function imprimirAsientos() {
                     circulo.setAttribute("onclick", `seleccionarAsiento('${circulo.id}')`);
 
                     circulo.innerText = asiento.numero
-                    switch (asiento.fila) {
-                        case 'A':
-                            filaA.appendChild(circulo);
-                            break;
-                        case 'B':
-                            filaB.appendChild(circulo);
-                            break;
-                        case 'C':
-                            filaC.appendChild(circulo);
-                            break;
-                        case 'D':
-                            filaD.appendChild(circulo);
-                            break;
-                        case 'E':
-                            filaE.appendChild(circulo);
-                            break;
-                        case 'F':
-                            filaF.appendChild(circulo);
-                            break;
-                        case 'G':
-                            filaG.appendChild(circulo);
-                            break;
-                        case 'H':
-                            filaH.appendChild(circulo);
-                            break;
-                        case 'I':
-                            filaI.appendChild(circulo);
-                            break;
-                        case 'J':
-                            filaJ.appendChild(circulo);
-                            break;
-                        case 'K':
-                            filaK.appendChild(circulo);
-                            break;
-                        case 'L':
-                            filaL.appendChild(circulo);
-                            break;
-                        case 'M':
-                            filaM.appendChild(circulo);
-                            break;
-                        case 'N':
-                            filaN.appendChild(circulo);
-                            break;
-                        case 'O':
-                            filaO.appendChild(circulo);
-                            break;
-                        case 'P':
-                            filaP.appendChild(circulo);
-                            break;
-                        case 'Q':
-                            filaQ.appendChild(circulo);
-                            break;
-                        case 'R':
-                            filaR.appendChild(circulo);
-                            break;
-                        case 'S':
-                            filaS.appendChild(circulo);
-                            break;
-                        case 'T':
-                            filaT.appendChild(circulo);
-                            break;
-                        case 'U':
-                            filaU.appendChild(circulo);
-                            break;
-                        case 'V':
-                            filaV.appendChild(circulo);
-                            break;
-                        case 'W':
-                            filaW.appendChild(circulo);
-                            break;
-                        case 'X':
-                            filaX.appendChild(circulo);
-                            break;
-                        case 'Y':
-                            filaY.appendChild(circulo);
-                            break;
-                        case 'Z':
-                            filaZ.appendChild(circulo);
-                            break;
-                        default:
-                            console.log('No se encuentra la fila');
-                    }
+                    mapa.appendChild(circulo);
                     if (asiento.comprado === true) {
                         circulo.style.backgroundColor = "#1F293D"
                         circulo.style.cursor = "not-allowed"
